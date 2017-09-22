6a2042209c2f18d0a6575bd5cfcb324f2314e431
Jose Morales <jose@vauxoo.com>
2017-09-21 23:21:59 -0500

    [MIG] Modified the libraries used because these are not available in python3

--- website_logo/controllers/main.py
+++ website_logo/controllers/main.py
@@ -18,13 +18,13 @@
 #
 ##############################################################################
 
-from openerp.addons.web.controllers.main import Binary
-from openerp.addons.web import http
-from openerp.addons.web.http import request
-import openerp
-from openerp.modules import get_module_resource
 import functools
-from cStringIO import StringIO
+import io
+from odoo.addons.web.controllers.main import Binary
+from odoo.addons.web import http
+from odoo.addons.web.http import request
+import odoo
+from odoo.modules import get_module_resource
 
 db_monodb = http.db_monodb
 
@@ -43,13 +43,13 @@ class website_logo(Binary):
         elif dbname is None:
             dbname = db_monodb()
         if not uid:
-            uid = openerp.SUPERUSER_ID
+            uid = odoo.SUPERUSER_ID
         if uid and dbname:
             placeholder = functools.partial(
                 get_module_resource, 'web', 'static', 'src', 'img')
             try:
                 # create an empty registry
-                registry = openerp.modules.registry.Registry(dbname)
+                registry = odoo.modules.registry.Registry(dbname)
                 with registry.cursor() as cr:
                     cr.execute("""SELECT c.website_logo, c.write_date
                                     FROM res_users u
@@ -59,7 +59,7 @@ class website_logo(Binary):
                                """, (uid,))
                     row = cr.fetchone()
                     if row and row[0]:
-                        image_data = StringIO(str(row[0]).decode('base64'))
+                        image_data = io.StringIO(str(row[0]).decode('base64'))
                         response = http.send_file(
                             image_data, filename=imgname, mtime=row[1])
                         return response
